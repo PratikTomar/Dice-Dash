d60b56fc113fcd5a7d3a63c86f604d1112043e7a
PratikTomar <pratiksingh067@gmail.com>
2023-10-17 18:59:11 +0530

    Removed Comment and console log

--- src/components/DiceGame.js
+++ src/components/DiceGame.js
@@ -15,10 +15,6 @@ const DiceGame = () => {
     }
   };
 
-  // console.log(rollDice);
-  // Math.random will give values like 0.1, 0.2,0.3 meaning floating value.This will generate values to 0 to 1 (0 is inclusive but 1 is exclusive)
-  // So we will use Math.floor but this will give 0 as well thus we will add +1 to it
-
   return (
     <div className="dice-container">
       <h1>Dice Dash: Roll and Win</h1>
